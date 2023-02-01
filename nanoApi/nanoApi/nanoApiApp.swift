e8d8832e274d37bb14583cd6c26b9e0ce9f31547
Matheus Zanivan <zanivan@Matheuss-MacBook-Pro.local>
2023-02-01 14:53:34 -0300

    testSignature

---
+++ nanoApi/nanoApi/nanoApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  nanoApiApp.swift
+//  nanoApi
+//
+//  Created by Matheus Zanivan on 01/02/23.
+//
+
+import SwiftUI
+
+@main
+struct nanoApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
